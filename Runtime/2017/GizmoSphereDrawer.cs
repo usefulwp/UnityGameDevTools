302fd9683d94e4d9ffff734ce6153c81b7be6352
wangpeng <96868552+usefulwp@users.noreply.github.com>
2025-07-23 16:34:42 +0800

    Add files via upload

---
+++ Runtime/2017/GizmoSphereDrawer.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// 在场景视图中绘制一个球体的 Gizmo。
+/// </summary>
+public class GizmoSphereDrawer : MonoBehaviour
+{
+    public Color sphereColor = Color.green; // 球颜色
+    public float radius = 0.5f;             // 球半径
+    public bool wireframe = false;          // 是否是线框球
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = sphereColor;
+
+        if (wireframe)
+            Gizmos.DrawWireSphere(transform.position, radius); // 线框球
+        else
+            Gizmos.DrawSphere(transform.position, radius);      // 实心球
+    }
+}
